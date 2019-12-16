76121acbdf084d8761e5ac55d7bf8c3e3e38c0bf
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2019-12-16 22:54:03 +0100

    upgrade_media

---
+++ dist/js/settings.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+
+export const select = {
+  all: {
+    activeSection: '.page.active',
+  },
+  listOf: {
+    menuElements: '.list-menu a',
+  },
+  data: {
+    section: 'data-section',
+  },
+  hamburger: '.fas--background',
+  sidebar: '.sidebar',
+  main: '.container-main',
+};
+
+export const className = {
+  active: 'active',
+  deactive: 'deactive',
+  splash: {
+    one: 'splash_one',
+    two: 'splash_two',
+    three: 'splash_three',
+  }
+};
